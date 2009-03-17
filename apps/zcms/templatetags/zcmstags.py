eefa13f40777ba1642c0b1c95b8f85e705d13a11
Matthew Pontefract <mp@billie.(none)>
2009-03-17 21:57:44 +0000

    wip

--- apps/zcms/templatetags/zcmstags.py
+++ apps/zcms/templatetags/zcmstags.py
@@ -5,31 +5,50 @@ so we provide zextends. """
 from django.template import Template, Node, Library
 from django.template.loader_tags import ExtendsNode
 from zcms.cmstags import renderComponent
-from zcms import CMSContext
 register = Library()
 
-class ZextendsNode(Node):
-    def __init__(self, nodelist, parent_name_expr, name):
+class ZExtendsNode(Node):
+    def __init__(self, nodelist, contextNameExpr, templateName = None, templateVar = None):
         Node.__init__(self)
         self.nodelist = nodelist
-        self.parent_name_expr = parent_name_expr
-        self.name = name
+        self.contextNameExpr = contextNameExpr
+        self.templateName = templateName
+        self.templateVar = templateVar
 
     def render(self, context):
-        # hard code context !!!
-        cmscontext = CMSContext(language_iso='en_gb', channel_id=1)
-        component = renderComponent(cmscontext, self.name)
+        if self.templateVar:
+            name = context[self.templateVar]
+        else:
+            name = self.templateName
+        component = renderComponent(name)
         context['zcms_internal_component_'] = Template(component)
-        en = ExtendsNode(self.nodelist, None, self.parent_name_expr)
+        en = ExtendsNode(self.nodelist, None, self.contextNameExpr)
         return en.render(context)
 
-def do_zextends(parser, tokens):
+def do_zextends(parser, token):
     """ Use only {% zextends template-name %}. Retrieves and processes template 
-then passes to Django's 'extends' tag processor. """
+then passes to Django's 'extends' tag processor. 
 
-    bits = tokens.contents.split()
+Similar behaviour to built-in extends tag. It tag may be used in two ways: 
+``{% zextends "base" %}`` (with quotes)
+uses the literal value "base" as the name of the parent template to extend,
+or ``{% zextends variable %}`` uses the value of ``variable`` as the
+name of the parent template to extend.
+"""
+    bits = token.contents.split()
+    if len(bits) != 2:
+        raise TemplateSyntaxError, "'%s' takes one argument" % bits[0]
+    templateName, templateVar = None, None
+    if bits[1][0] in ('"', "'") and bits[1][-1] == bits[1][0]:
+        templateName = bits[1][1:-1]
+    else:
+        templateVar = bits[1]
+    # the contextNameExpr is needed by the Django ExtendsNode - haven't looked into
+    # making this less hacky - room for improvement!
+    contextNameExpr = parser.compile_filter("zcms_internal_component_")
     nodelist = parser.parse()
-    parent_name_expr = parser.compile_filter('zcms_internal_component_')
-    return ZextendsNode(nodelist, parent_name_expr, bits[1])
+    if nodelist.get_nodes_by_type(ZExtendsNode):
+        raise TemplateSyntaxError, "'%s' cannot appear more than once in the same template" % bits[0]
+    return ZExtendsNode(nodelist, contextNameExpr, templateName, templateVar)
 
 register.tag("zextends", do_zextends)
